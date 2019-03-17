956941fbff37d7f9b56bff482bd45e95c28180c4
mikolaj6r <mikolaj6r@gmail.com>
2019-03-18 00:36:22 +0100

    refactor

--- mouse.cpp
+++ mouse.cpp
@@ -9,44 +9,41 @@ int main()
     POINT p;
     double positionLabelX[129];
     double positionLabelY[129];
-    /////////////////////////////////////////////// odczyt pozycji
-
     int x, y;
+    double k[129];
+    double r[129];
+    std::ofstream myfile;
+
+    /////////////////////////////////////////////// odczyt pozycji
     std::cout<<"Poruszaj myszka"<<std::endl;
-    for(int i=0; i<129; i++)
+    for(int i=0; i<129;)
     {
         Sleep(20);
         GetCursorPos(&p);
         if(x==p.x && y==p.y)
-            i--;
+            continue;
         else
         {
-            x=p.x;
-            y=p.y;
-            positionLabelX[i]=x;
-            positionLabelY[i]=y;
+            positionLabelX[i] = x = p.x;
+            positionLabelY[i] = y = p.y;
         }
+        i++;
     }
 
     /////////////////////////////////////////////// obliczenia
-    double k[129];
-    double r[129];
     for(int i=0; i<128; i++)
     {
         if(positionLabelX[i]==positionLabelX[i+1])
-        {
             k[i]=1.57;
-        }
         else
-        {
             k[i]=atan((abs(positionLabelY[i+1]-positionLabelY[i])/abs(positionLabelX[i+1]-positionLabelX[i])));
-        }
+
         r[i]=(k[i]/1.57);
+
         std::cout<<"x = "<<positionLabelX[i]<<"   y = "<<positionLabelY[i]<<"   kat = "<<k[i]<<"   r = "<<r[i]<<std::endl;
     }
 
-    /////////////////////////////////////////////// zapis do pliku wyników
-    std::ofstream myfile;
+    /////////////////////////////////////////////// zapis do pliku wynikow
     myfile.open("position.txt");
     for(int i=0; i<128; i++)
     {
